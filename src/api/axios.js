60293fcb8fb87bf601d8192935b786e6d4b010e2
Pranav Abhimanyu <146646970+dubeypranaviiit@users.noreply.github.com>
2026-01-10 21:05:01 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
 
 api.interceptors.response.use(
   (res) => res,
@@ -11,5 +12,6 @@ api.interceptors.response.use(
     return Promise.reject(err);
   }
 );
-
 export default api;
+
+
